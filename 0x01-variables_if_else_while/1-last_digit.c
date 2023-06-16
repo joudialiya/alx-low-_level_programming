630e136d044edd649e91ef886ad1522514bf3a23
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-16 13:14:35 -0700

    fix, 1., logic crisis

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of %d is %d and ", n, n % 10 );
-	if(n % 10 < 6)
-		printf("is less than 6 and not 0");
-	else if (n % 10 == 0)
+	if(n % 10 == 0)
 		printf("is 0");
+	else if (n % 10 < 6)
+		printf("is less than 6 and not 0");
 	else
 		printf("is greater than 5");	
 	putchar('\n');
