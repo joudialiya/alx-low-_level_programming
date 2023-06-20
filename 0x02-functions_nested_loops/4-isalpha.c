aa6078c59803ae96bd2462aaba2a1d196e02fb3f
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-20 09:59:18 -0700

    fix, 4., logic crisis lol

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,9 +10,9 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'A' || c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
