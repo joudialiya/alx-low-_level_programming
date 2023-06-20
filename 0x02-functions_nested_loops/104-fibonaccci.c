669139eb5514cf204ad45a031818ebae48b6cb7a
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-20 15:02:54 -0700

    16.

---
+++ 0x02-functions_nested_loops/104-fibonaccci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - fibounacci
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int n0 = 1;
+	unsigned long int n1 = 2;
+	unsigned long int tmp = 0;
+	int index = 2;
+
+	printf("%lu, ", n0);
+	printf("%lu, ", n1);
+	while (index < 50)
+	{
+		printf("%lu", n0 + n1);
+		tmp = n0;
+		n0 = n1;
+		n1 += tmp;
+		if (index != 49)
+			printf(", ");
+		++index;
+	}
+	printf("\n");
+	return (0);
+}
