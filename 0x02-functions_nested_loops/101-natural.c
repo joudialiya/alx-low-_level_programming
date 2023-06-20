ecff8436dfb88ad2d9554c4ffa128cc382c3f500
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-20 14:34:08 -0700

    13.

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - natural
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+	int result = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			result += i;
+		}
+		++i;
+	}
+	printf("%d", result);
+	return (0);
+}
