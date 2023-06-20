0ee4ebc476ecaa827b6bd085bee5a7142af6b4ea
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-20 14:01:00 -0700

    fix, 11., some display issues

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,16 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			if (n != 98)
-				printf(",");
+				printf(", ");
 			++n;
 		}
 	else
-		while (n > 98)
+		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
-				printf(",");
+				printf(", ");
 			--n;
 		}
-			
 	putchar('\n');
 }
