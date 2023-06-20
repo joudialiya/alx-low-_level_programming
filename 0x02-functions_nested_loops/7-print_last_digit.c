1922eebd36ceee6e12b4cfff18bd3e3cfb3f6252
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-20 13:05:03 -0700

    forgot about the negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_last_digit - print the last digit
  * @n: the number
@@ -8,6 +8,6 @@
  */
 int print_last_digit(int n)
 {
-	_putchar ('0' + (n % 10));
-	return (n % 10);
+	_putchar ('0' + ((n > 0) ? (n % 10) : -(n % 10)));
+	return ((n > 0) ? (n % 10) : -(n % 10));
 }
