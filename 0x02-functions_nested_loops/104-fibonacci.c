e145bab63c0a770984c67df93af285ccc154dfa2
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-20 17:57:35 -0700

    ana m9awd salat, manipulating decimals like binary, segmentation is the key

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#define DEL 1000000000
+
+void print_n(unsigned long part1, unsigned long part2)
+{
+	if(part2 == 0)
+		printf("%lu", part1);
+	else
+		printf("%lu%09lu", part2, part1);
+}
 
 /**
  * main - fibounacci
@@ -7,19 +16,37 @@
  */
 int main(void)
 {
-	unsigned long int n0 = 1;
-	unsigned long int n1 = 2;
-	unsigned long int tmp = 0;
+	/* part2-part1 */
+	unsigned long int n0_part1 = 1;
+	unsigned long int n0_part2 = 0;
+
+	unsigned long int n1_part1 = 2;
+	unsigned long int n1_part2 = 0;
+
+	unsigned long int tmp_part1 = 0;
+	unsigned long int tmp_part2 = 0;
 	int index = 2;
 
-	printf("%lu, ", n0);
-	printf("%lu, ", n1);
+	print_n(n0_part1, n0_part2);
+	printf(", ");
+	print_n(n1_part1, n1_part2); 
+	printf(", ");
 	while (index < 98)
 	{
-		printf("%lu", n0 + n1);
-		tmp = n0;
-		n0 = n1;
-		n1 += tmp;
+		tmp_part1 = n0_part1 + n1_part1;
+		tmp_part2 = n0_part2 + n1_part2;
+		if (tmp_part1 / DEL != 0)
+		{
+			tmp_part1 %= DEL;
+			tmp_part2 += 1;
+		
+		}
+		print_n(tmp_part1, tmp_part2);
+		n0_part1 = n1_part1;
+		n0_part2 = n1_part2;
+
+		n1_part1 = tmp_part1;
+		n1_part2 = tmp_part2;
 		if (index != 97)
 			printf(", ");
 		++index;
