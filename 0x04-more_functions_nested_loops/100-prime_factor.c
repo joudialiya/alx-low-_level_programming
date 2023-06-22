49164c8737014c80b02458ba1882bb93615f13c9
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-22 16:21:14 -0700

    [tast 11, had to google the prime factors]

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - largest prime factor
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	unsigned long n = 612852475143;
+	unsigned long fact = 2;
+	unsigned long max = 0;
+
+	while (n > 1)
+	{
+		while (n % fact == 0)
+		{
+			n /= fact;
+			if (fact > max)
+				max = fact;
+		}
+		fact++;
+	}
+
+	printf("%lu\n", max);
+	return (0);
+}
