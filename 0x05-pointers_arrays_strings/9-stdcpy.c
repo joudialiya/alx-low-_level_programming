2062b81ca7fe21e3f21efe9cc85a5b3cab8d2cf3
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-06-26 05:46:31 -0700

    [task 9]

---
+++ 0x05-pointers_arrays_strings/9-stdcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - cpy a str
+ * @dest: destination
+ * @src: source
+ *
+ * Return: @dest
+ */
+char * _strcpy(char * dest, char * src)
+{
+	char *dest_p = dest;
+	char *src_p = src;
+
+	while (*src_p != '\0')
+	{
+		*dest_p = *src_p;
+		++dest_p;
+		++src_p;
+	}
+
+	*dest_p = *src_p;
+
+	return dest;
+}
