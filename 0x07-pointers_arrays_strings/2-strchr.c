ed417bec2f183f537f3b68a068ee9d782c5e1e7f
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-03 03:47:08 -0700

    [task 2]

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - occurrence
+ * @s: str
+ * @c: char
+ *
+ * Return: ptr
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != 0)
+	{
+		if (*s == c)
+			return (s);
+		++s;
+	}
+
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
 #endif
