edd5767b59f1d4d55869e06f636422d300bad063
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-03 13:58:08 -0700

    [task 12, print a number]

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,13 +8,14 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
 	}
+	else
+		n = -n;
 
 	if (n / 10 != 0)
-		print_number(n / 10);
+		print_number(- n / 10);
 
-	_putchar('0' + n % 10);
+	_putchar('0' - n % 10);
 
 }
