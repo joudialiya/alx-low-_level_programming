3b26363a1f3d3afcd3bea8f5e323ca753a5769a4
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-03 03:23:28 -0700

    [task 0, memset]

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - set memory
+ * @s: buffer
+ * @c: the byte
+ * @n: buffer's size
+ *
+ * Return: s
+ */
+char *_memset(char *s, char c, unsigned int n)
+{
+	char *ptr = s;
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = c;
+		++i;
+	}
+
+	return (s);
+}
