1bcc53a32175f42343fcd115505d2cdd60bb83cf
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-05 15:46:28 -0700

    [task 8]

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,7 +25,7 @@ int wildcmp(char *s1, char *s2)
 			return (1);
 		if (*(s2 + 1) != *s1)
 			return (wildcmp(s1 + 1, s2));
-		return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	if (*s2 != '*')
 	{
