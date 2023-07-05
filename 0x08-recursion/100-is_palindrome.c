d18e1f9b626e0588f74f4b2d906a90ca273e23fb
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-05 06:29:10 -0700

    [task 7]

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+/**
+ * helper - palindrome helper
+ * @start: start of the str
+ * @end: the end [befor '\0']
+ *
+ * Return: 0 or 1
+ */
+int helper(char *start, char *end)
+{
+	if (start > end)
+		return (1);
+	if (*start != *end)
+		return (0);
+	helper(start + 1, end - 1);
+}
+/**
+ * ticket_to_the_end - reach the end of a string
+ * @s: string
+ *
+ * Return: ptr
+ */
+char *ticket_to_the_end(char *s)
+{
+	if (*(s + 1) == 0)
+		return (s);
+	return (ticket_to_the_end(s + 1));
+}
+
+/**
+ * is_palindrome - checks for a palindrom
+ * @s: string
+ *
+ * Return: 0 or 1
+ */
+int is_palindrome(char *s)
+{
+	return (helper(s, ticket_to_the_end(s)));
+}
