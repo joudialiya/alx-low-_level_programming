ba93afffa3f9fcd9dea75e62608515a33607a6c3
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-05 06:11:48 -0700

    [task 6]

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * prime_helper - if prime
+ * @index: index
+ * @n : num
+ *
+ * Return: if prime
+ */
+int prime_helper(int index, int n)
+{
+
+	if (n % index == 0)
+		return (0);
+	if (index == n)
+		return (1);
+	return (prime_helper(index + 1, n));
+}
+
+/**
+ * is_prime_number - is prime
+ * @n: num
+ *
+ * Return: if prime
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+		n = -n;
+
+	if (n == 1 || n == 0)
+		return (0);
+
+	return (prime_helper(2, n));
+}
