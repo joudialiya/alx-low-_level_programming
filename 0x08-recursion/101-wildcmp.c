793aba80e9e5f287daea1e3f99a567f05b9c9ec8
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-05 07:07:33 -0700

    [task 8]

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings with a twist
+ * @s1: str 1
+ * @s2: str 2 may comain the * char that reppresent any string
+ *
+ * Return: 1 or 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+}
+
