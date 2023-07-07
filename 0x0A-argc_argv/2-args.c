c0b5f85941cca96da8002c9d1be28a98eb53107d
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-07 04:55:04 -0700

    [task 2]

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - display
+ * @argc: agr count
+ * @argv: arg values
+ *
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	char *ptr;
+
+	while (i < argc)
+	{
+		ptr = argv[i];
+		while (*ptr != 0)
+		{
+			_putchar(*ptr);
+			++ptr;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
