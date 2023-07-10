cdde8d5fbdaa66ad0fd08d58b6660c4b7394257d
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-10 06:35:26 -0700

    [task 3]

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * alloc_grid - alloc a int grid
+ * @w: width
+ * @h: heigth
+ *
+ * Return: ptr;
+ */
+
+void alloc_grid(int w, int h)
+{
+	int *ptr = 0;
+	int **p = 0;
+	int i = 0;
+
+	if (w <= 0 || h <= 0)
+		return (0);
+
+	ptr = (int *)malloc(sizeof(int) * w * h);
+	p = (int **)malloc(sizeof(int *) * h);
+
+	while (i < h)
+	{
+		p[i] = ptr +  i * w;
+		++i;
+	}
+
+	return (p);
+}
