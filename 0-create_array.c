48e7968828c6875ddd05f575a7e59e8bbb6f7de9
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-10 05:24:59 -0700

    [task 0]

---
+++ 0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - creat array and init it
+ * @size:  size of the array
+ * @c: char
+ *
+ * Return: ptr
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *ptr =  malloc(sizeof(char) * size);
+
+	while (i < sizeof(char) * size)
+	{
+		ptr[i] = c;
+		++i;
+	}
+}
