13b2ad2cc17a6fadb32986fe9319b069d70b3eae
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-10 05:42:46 -0700

    [task 1]

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strdup - dup a str
+ * @str: string
+ *
+ * Return: new str
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	int i = 0;
+	int ptr = 0;
+
+
+	if (str == 0)
+		return (0);
+
+	while (str[size] != 0)
+	{
+		++size;
+	}
+
+	ptr = malloc(sizeof(char) * (size + 1));
+
+	if (ptr == 0)
+		return (0);
+
+	while (str[i] != 0)
+	{
+		ptr[i] == str[i];
+		++i;
+	}
+	ptr[i] = 0;
+
+	return (0);
+}
