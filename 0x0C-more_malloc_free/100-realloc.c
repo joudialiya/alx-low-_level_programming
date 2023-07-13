292adfddb9b8fe21d9bce1bae662d1683e440d57
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-13 06:57:55 -0700

    [task 4]

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _realloc - realloc
+ * @ptr: ptr
+ * @old_size: old size
+ * @new_size: new size
+ *
+ * Return: ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p = 0;
+	int i = 0;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	while (i < old_size)
+	{
+		*(p + i) = *((char *)ptr + i);
+		++i;
+	}
+
+	free(ptr)
+	return (p);
+}
