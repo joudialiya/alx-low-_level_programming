0643909235ea9ef8cea9b8b0be642cfefd96ec11
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-17 05:41:30 -0700

    [task 4]

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,63 @@
+#include "dog.h"
+
+/**
+ * _strlen - strlen copy
+ * @str: str
+ *
+ * Return: size
+ */
+int _strlen(char *str)
+{
+	if (*str == 0)
+		return (0);
+	return (1 + _strlen(str + 1));
+}
+/**
+ * _strdup - strdup copy
+ * @str: str
+ *
+ * Return: ptr
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *ptr = 0;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	size = _strlen(str);
+	ptr = malloc(sizeof(char) * (size + 1));
+	ptr[size] = '\0';
+
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		++i;
+	}
+	return (ptr);
+}
+
+
+/**
+ * new_dog - gic constructor
+ * @name: name
+ * @age: age
+ * @owner: name of the owner
+ *
+ * Return: ptr to the new dog;
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = NULL;
+
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
+	d->name = _strdup(name);
+	d->age = age;
+	d->owner = _strdup(owner);
+
+	return (d);
+}
