292436e7839155331085737e09caa4034d34ae82
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-17 05:06:52 -0700

    [task 0]

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog stuct
+ * @name: name
+ * @age: age
+ * @owner: name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
