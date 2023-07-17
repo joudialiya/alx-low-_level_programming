ab6ae38a591b59daa1f511ba777aad2a6e0b7367
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-17 05:22:56 -0700

    [task 1]

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,55 @@
 #include "dog.h"
 
 /**
+ * _strlen - strlen copy
+ * @str: str
+ *
+ * Return: size
+ */
+int _strlen(char *str)
+{
+	if (*str == 0)
+		return (0);
+	return (1 + _strlen(str + 1));
+}
+/**
+ * _strdup - strdup copy
+ * @str: str
+ *
+ * Return: ptr
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *ptr = 0;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	size = _strlen(str);
+	ptr = malloc(sizeof(char) * (size + 1));
+	ptr[size] = '\0';
+
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		++i;
+	}
+	return (ptr);
+}
+
+
+/**
  * init_dog - gic constructor
  * @d: ptr
  * @name: name
  * @age: age
  * @owner: name of the owner
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	d->name = _strdup(name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = _strdup(owner);
 }
