974a338a409f1781e4e9ced4a0e2d9bf246a88d3
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-17 05:36:05 -0700

    [task 2]

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct dog - a dog stuct
  * @name: name
@@ -13,6 +15,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *, char *, float, char *);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
