cc6a171c7aca35e13ae511f308f23880e54173b3
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-07-20 11:56:52 -0700

    [task 3]

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,22 +10,27 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i = 0;
 	char *str = NULL;
+	int valid = 0;
 
 	va_start(args, format);
 	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
-			case ('c'):
+			case ('c'):			
+				valid = 1;
 				printf("%c", va_arg(args, int));
 			break;
 			case ('i'):
+				valid = 1;
 				printf("%d", va_arg(args, int));
 			break;
 			case ('f'):
+				valid = 1;
 				printf("%f", va_arg(args, double));
 			break;
 			case ('s'):
+				valid = 1;
 				str = va_arg(args, char *);
 				if (str)
 				{
@@ -35,8 +40,9 @@ void print_all(const char * const format, ...)
 				printf("(nil)");
 			break;
 		}
-		if (format[i + 1] != '\0')
+		if (format[i + 1] != '\0' && valid)
 			printf(", ");
+		valid = 0;
 		++i;
 	}
 
