a08ea94b692894530653b601da8f672bf34ab4e5
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-08-01 03:55:26 -0700

    [task 9]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,41 +12,41 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr = NULL;
 	listint_t *curr = NULL;
-	int i = 0;
+	unsigned int i = 0;
 
-	if (idx < 0)
+	if (head == NULL)
 		return (NULL);
-
 	ptr = malloc(sizeof(listint_t));
-
 	if (!ptr)
 		return (NULL);
-
 	ptr->n = n;
 	ptr->next = NULL;
 	curr = *head;
 	/* Meaning that the list is empty */
-	if (curr == NULL)
+	if (idx == 0)
 	{
+		ptr->next = *head;
 		*head = ptr;
-		return (ptr);
 	}
-	/* Meaning thet the list has some elements */
-	while (i != index - 1 && curr != NULL)
+	else
 	{
-		curr = curr->next;
-		++i;
+		/* Meaning thet the list has some elements */
+		while (i != idx - 1 && curr != NULL)
+		{
+			curr = curr->next;
+			++i;
+		}
+		/**
+		* meaning that we are at the end of the list
+		* the index exide the list
+		*/
+		if (curr == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		ptr->next = curr->next;
+		curr->next = ptr;
 	}
-	/**
-	 * meaning that we are at the end of the list
-	 * the index exide the list
-	 */
-	if (curr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	ptr->next = curr->next;
-	curr->next = ptr;
 	return (ptr);
 }
