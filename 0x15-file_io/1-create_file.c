e64e9d684a89b4d6715739920bb4cdc252812bff
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-08-07 13:49:49 -0700

    [task 1, create file]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,24 +5,43 @@
  * @filename: filename
  * @text_content
  *
- * Return: 1 or -1 
+ * Return: 1 or -1
  */
-ssize_t create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
-	int r = 0;
+	int w = 0;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+
+	if (fd < 0)
 		return (-1);
 
 	if (text_content == NULL)
 		return (1);
 
-	r = write(fd, STDOUT_FILE, _strlen(text_content));
+	w = write(fd, text_content, _strlen(text_content));
 
-	if (r == -1)
+	if (w < 0)
 		return (-1);
 
 	return (1);
+}
+
+/**
+ * _strlen - strlen cpy
+ * @str: string
+ *
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int r = 0;
+
+	while (*str != 0)
+	{
+		++r;
+		++str;
+	}
+	return (r);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #define EOF (-1)
 
 int _putchar(int);
+int _strlen(char *str);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
