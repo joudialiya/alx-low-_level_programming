81b4e88fa8c027e7f6d2a53c35ec74faa2b8f8db
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-08-07 13:27:56 -0700

    [task 0]

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,23 +11,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = 0;
 	ssize_t r = 0;
-	char buf[1024];
+	ssize_t w = 0;
+	char buf[2048];
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fd = open(filename, O_RDWR);
+
+	if (fd < 0)
 		return (0);
 
 	r = read(fd, buf, letters);
 
-	if (r <= 0)
+	if (r < 0)
 		return (0);
 
-	r = write(STDOUT_FILENO, buf, (size_t)r);
+	w = write(STDOUT_FILENO, buf, (size_t)r);
 
-	if (r < 0)
+	if (w < 0 || r != w)
 		return (0);
 
 	return (r);
