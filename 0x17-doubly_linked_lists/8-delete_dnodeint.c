d22c63b80c56f597257270cb36c7e2818dbf5ffd
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-14 06:11:05 -0700

    task 8, if the *head is NULL why should we return, is it a successfull operation?

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,6 +12,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (head == NULL)
 		return (-1);
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		dlistint_t *next = NULL;
