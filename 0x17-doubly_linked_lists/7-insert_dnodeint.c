5f5fb43be46a11f9ce32658a40f461f2dbc5685c
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-14 06:19:20 -0700

    task 7, betty check

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,33 +14,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
-			return (add_dnodeint(h, n));
-	else
-	{
-		current = *h;
-		while (current && idx > 0)
-		{
-			current = current->next;
-			--idx;
-		}
-		if (idx == 0)
-		{
-			if (current)
-			{
-				dlistint_t *node = add_dnodeint(&current, n);
+		return (add_dnodeint(h, n));
 
-				if (node)
-				{
-						current->prev->next = node;
-				}
-				return (node);
-			}
-			else
-			{
-				return (add_dnodeint_end(h, n));
-			}
+	current = *h;
+	while (current && idx > 0)
+	{
+		current = current->next;
+		--idx;
+	}
+	if (idx == 0)
+	{
+		if (current)
+	{
+		dlistint_t *node = add_dnodeint(&current, n);
+
+		if (node)
+		{
+			current->prev->next = node;
+		}
+		return (node);
 		}
 		else
-			return (NULL);
+		{
+			return (add_dnodeint_end(h, n));
+		}
 	}
+	else
+		return (NULL);
 }
