319291c05e4d1d11028e1f6f262044e42d563fff
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-18 11:33:46 -0700

    task 3, some cleanup

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/rand.so https://github.com/joudialiya/alx-low_level_programming/tree/master/0x18-dynamic_libraries/rand.so
+wget -O /tmp/rand.so https://raw.githubusercontent.com/joudialiya/alx-low_level_programming/master/0x18-dynamic_libraries/rand.so 
 export LD_PRELOAD=/tmp/rand.so
