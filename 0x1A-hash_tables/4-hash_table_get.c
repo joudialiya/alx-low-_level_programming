ae94231b4404df4dd1bb7434b8b86ed41e918231
joudialiya <mlagzal.sayhi@gmail.com>
2023-10-05 22:55:42 +0000

    4. handling the case of ht == NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *entry = NULL;
 
+	if (!ht)
+		return (NULL);
 	entry = ht->array[key_index((const unsigned char *)key, ht->size)];
 	while (entry)
 	{
