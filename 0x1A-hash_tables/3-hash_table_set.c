11e5270b39badcceecfe9c0733fda0ecd420ea69
joudialiya <mlagzal.sayhi@gmail.com>
2023-10-05 18:50:48 +0000

    3. handling the case when we have same keys

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,32 +11,40 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t **entry = NULL;
 	hash_node_t *element = NULL;
+	hash_node_t *current = NULL;
 
 	if (!ht || !key || strlen(key) == 0)
 		return (0);
 
 	entry = &(ht->array[key_index((const unsigned char *)key, ht->size)]);
 	element = malloc(sizeof(hash_node_t));
-
 	if (!element)
 		return (0);
-	element->key = NULL;
-	element->value = NULL;
-	element->next = NULL;
+	element->key = NULL, element->value = NULL, element->next = NULL;
 	element->key = strdup(key);
 	if (value)
 		element->value = strdup(value);
 	if (!key || !value)
 	{
-		free(element->key);
-		free(element->value);
-		free(element);
+		free(element->key), free(element->value), free(element);
 		return (0);
 	}
 	if (!(*entry))
 	{
 		(*entry) = element;
 		return (1);
+	}
+	current = *entry;
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(element->key), free(element->value), free(element);
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
 	}
 	element->next = (*entry);
 	(*entry) = element;
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -15,8 +15,8 @@ int main(void)
 
     ht = hash_table_create(1024);
     hash_table_set(ht, "hetairas", "a");
-    hash_table_set(ht, "mentioner", "b");
-	v = ht->array[key_index((const unsigned char *)"mentioner", 1024)]->next->value;
+    hash_table_set(ht, "hetairas", "b");
+	v = ht->array[key_index((const unsigned char *)"mentioner", 1024)]->value;
 	printf("%s\n", v);
     return (EXIT_SUCCESS);
 }
