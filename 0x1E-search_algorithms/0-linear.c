92e35188f1eaba8f4b03e4e958dbe49a45c3836e
Lagzal <mlagzal.sayhi@gmail.com>
2024-07-20 14:50:03 +0000

    0.

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,9 +10,10 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	for (int i = 0; i < size; ++i)
+	size_t i = 0;
+	for (i = 0; i < size; ++i)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
